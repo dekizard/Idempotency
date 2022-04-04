431249615d7fedd1bb75de74aaa366b2e72be5f1
Dejan Sinik <mijat8ds@yahoo.com>
2022-04-05 00:22:06 +0200

    Removed unnecessary folders

--- Idempotency.Integration.Interfaces/IDistributedCacheService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Idempotency.Integration.Interfaces;
-
-public interface IDistributedCacheService
-{
-    Task<byte[]> GetOrSet(string key, byte[] value);
-    Task Set(string key, byte[] value);
-    Task Remove(string key);
-}
